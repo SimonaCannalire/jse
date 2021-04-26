9bc7421d174975a0f6821c643c0fbcae32ca52ee
Simona Cannalire <simona.cannalire@studio.unibo.it>
2021-04-26 17:54:13 +0200

    example class

---
+++ src/main/java/m02/s07/Example.java
@@ -0,0 +1,12 @@
+package m02.s07;
+
+public class Example {
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			System.out.println("You passed me: " + args[0]);
+		}
+		System.out.println("Bye");
+		
+	}
+}
+
