417ff55857ea83599f90f4f490af2b17c87a2e4b
Simona Cannalire <simona.cannalire@studio.unibo.it>
2021-04-28 11:43:25 +0200

    classic for

--- src/main/java/m02/s10/ex/Exercise3.java
+++ src/main/java/m02/s10/ex/Exercise3.java
@@ -7,5 +7,11 @@ public class Exercise3 {
          * (1) the number of arguments passed to this main
          * (2) each argument
          */
+    	System.out.println("You passed me " + args.length + " argument(s)");
+    	
+    	for (int i=0; i<args.length; i++) {
+    		System.out.println(args[i]);
+    	}
     }
+    
 }
