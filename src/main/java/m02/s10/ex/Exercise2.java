0741255a657f42a0ae3138f983b6bf1cce6736a4
Simona Cannalire <simona.cannalire@studio.unibo.it>
2021-04-29 12:11:33 +0200

    exercise 2

--- src/main/java/m02/s10/ex/Exercise2.java
+++ src/main/java/m02/s10/ex/Exercise2.java
@@ -1,12 +1,18 @@
 package m02.s10.ex;
 
-import java.util.Arrays;
+import java.util.Arrays; //swap the arrays true-false.
 
 public class Exercise2 {
-    public static void main(String[] args) {
-        boolean[] flags = { true, false, false };
-        System.out.println(Arrays.toString(flags));
-
-        // TODO: swap the values in the given boolean array and then print it
-    }
+	public static void main(String[] args) {
+		boolean[] flags = { true, false, false };
+		System.out.println(Arrays.toString(flags));
+		for (int i = 0; i < flags.length; i++) {
+			if (flags[i] == true) {
+				flags[i] = false;
+			} else {
+				flags[i] = true;
+			}
+		}
+		System.out.println(Arrays.toString(flags));
+	}
 }
