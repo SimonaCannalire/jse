5c4e277bb6164d3276f0dd431af731be85969670
Simona Cannalire <simona.cannalire@studio.unibo.it>
2021-05-02 12:55:09 +0200

    new methods

--- src/main/java/mex/s02/Exercise1.java
+++ src/main/java/mex/s02/Exercise1.java
@@ -14,7 +14,7 @@ public class Exercise1 {
      */
     public static double speed(double distance, double time) {
         log.info(String.format("distance is %.3f and time is %.2f", distance, time));
-
-        return distance / time;
+                double speed = distance/time;
+                return speed;
     }
 }
