babb9fb2503fd839c3e6c048976dcdec4f6f9a41
Simona Cannalire <simona.cannalire@studio.unibo.it>
2021-05-05 12:58:30 +0200

    anagram

--- src/main/java/mex/s05/Exercise5.java
+++ src/main/java/mex/s05/Exercise5.java
@@ -1,5 +1,7 @@
 package mex.s05;
 
+import java.util.Arrays;
+
 public class Exercise5 {
 	/**
 	 * Check for anagram
@@ -9,9 +11,26 @@ public class Exercise5 {
 	 * @return true if s is an anagram of t
 	 */
 	public static boolean isAnagram(String s, String t) {
-		if (s == null && t == null || s.length() == 0 && s.length() == 0) {
+		if (s.length() != t.length()) {
 			return false;
-		
 		}
+//		char[] a = new char[s.length()];
+//		for (int i = 0; i < s.length(); i++) {
+//			a[i] = s.charAt(i);
+//		}
+//		char[] b = new char[t.length()];
+//		for (int j = 0; j < t.length(); j++) {
+//			b[j] = t.charAt(j);
+//		}
+		char[] a = s.toCharArray();
+		char[] b = t.toCharArray();
+		Arrays.sort(a);
+		Arrays.sort(b);
+//		if (Arrays.equals(a, b)) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+		return Arrays.equals(a, b);
 	}
 }
