55361842c02db4675fc534b81064467d9747cfc3
Simona Cannalire <simona.cannalire@studio.unibo.it>
2021-05-10 16:55:45 +0200

    last exercises

--- src/main/java/mex/s06/Exercise1a.java
+++ src/main/java/mex/s06/Exercise1a.java
@@ -8,10 +8,13 @@ public class Exercise1a {
      * @return a new array holding the same elements of input, in reversed order
      */
     public static int[] reverse(int[] data) {
-        int[] result = new int[0];
-
-        // TODO
-
+        if (data == null) {
+        	return null;
+        }
+    	int[] result = new int[data.length];
+    	for (int i = 0; i < data.length; i++) {
+    		result[i] = data[data.length - i -1];   		
+    	} //vado dall'ultimo elemento verso il primo      
         return result;
     }
 }
--- src/main/java/mex/s06/Exercise5.java
+++ src/main/java/mex/s06/Exercise5.java
@@ -1,14 +1,27 @@
 package mex.s06;
 
+import java.util.Arrays;
+
 public class Exercise5 {
-    /**
-     * Find the duplicated values
-     * 
-     * @param data
-     * @return the duplicated value
-     */
-    public static int[] duplicates(int[] data) {
-        // TODO
-        return new int[0];
-    }
+	/**
+	 * Find the duplicated values
+	 * 
+	 * @param data
+	 * @return the duplicated value
+	 */
+	public static int[] duplicates(int[] data) {
+		if (data == null || data.length == 0) {
+			return data;
+		}
+		int size = 0;
+		int[] result = new int[data.length];
+		Arrays.sort(data);
+		for (int i = 1; i < data.length; i++) {
+			if (data[i] == data[i - 1]) {
+				result[size] = data[i];
+				size += 1;
+			} // anche: result[size++] = data[i];
+		}
+		return Arrays.copyOf(result, size);
+	}
 }
